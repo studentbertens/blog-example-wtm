23143ecc505be8474c4bed7aa6db383aa9374fdd
Alejandro Ríos <alerios7@gmail.com>
2015-09-03 20:34:59 -0300

    Seed example

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+20.times do
+  title = Faker::Lorem.sentence
+  body = Faker::Lorem.paragraph
+  post = Post.create(title: title, body: body)
+  rand(5).times do
+    message = Faker::Lorem.paragraph
+    post.comments.create(message: message)
+  end
+end
+
+#20.times do
+
+#  id_post = rand(1..20)
+#  comment = Comment.create(message: message, post_id: id_post)
+#end
