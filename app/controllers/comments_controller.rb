06d582622ae861a4d3181b6394461a5036b2ea74
Luis Porras <lporras16@gmail.com>
2015-09-07 19:30:10 -0300

    destroy comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,24 +7,18 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.destroy
+      redirect_to post_path(@post), notice: "Se ha eliminado correctamente"
+    else
+      redirect_to post_path(@post), error: "No se ha podido eliminar"
+    end
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:message, :post_id)
     end
 end
-
-#  def destroy
-#     @post = Post.find(params[:post_id])
-#     @comment = @post.comments.find(params[:id])
-#     @comment.destroy
-#     redirect_to post_path(@post)
-#   end
-
-  def destroy
-    @comment = @comments.find params[:id]
-    if @comments.destroy
-      redirect_to comments_path, notice: "Se ha eliminado correctamente"
-    else
-      redirect_to comments_path(@comments), error: "No se ha podido eliminar"
-    end
-  end
